17a96c632c35a3d9e3a83279e0a803cbca0a26b9
vincci-cheng-project-development-page <vincci.cheng.github@gmail.com>
2021-09-08 16:36:38 +1000

    Completed Assignment

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -1,19 +1,32 @@
 require 'pry'
 
   # This is the array we will be passing into the remove_strawberry method
-  # contacts = {
-  #   "Jon Snow" => {
-  #     name: "Jon",
-  #     email: "jon_snow@thewall.we", 
-  #     favorite_ice_cream_flavors: ["chocolate", "vanilla"]
-  #   },
-  #   "Freddy Mercury" => {
-  #     name: "Freddy",
-  #     email: "freddy@mercury.com",
-  #     favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
-  #   }
-  # }
+  contacts = {
+     "Jon Snow" => {
+       name: "Jon",
+       email: "jon_snow@thewall.we", 
+       favorite_ice_cream_flavors: ["chocolate", "vanilla"]
+     },
+     "Freddy Mercury" => {
+       name: "Freddy",
+       email: "freddy@mercury.com",
+       favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
+     }
+   }
   
 def remove_strawberry(contacts)
-
+  contacts.each do |person, contact_details_hash|
+    #binding.pry to see what person and contact_details_hash equal to
+    if person == "Freddy Mercury"
+      contact_details_hash.each do |attribute, data|
+        #binding.pry to see what attribute and data equal to
+        if attribute == :favorite_ice_cream_flavors
+       data.delete_if {|ice_cream| ice_cream == 'strawberry'}
+      end
+    end  
+    end
+  end
 end
+
+
+p remove_strawberry(contacts)
